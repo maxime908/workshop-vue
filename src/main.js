7bf65f815595303aed375cba59f5e9733655e2eb
= <=>
2026-02-25 10:04:03 +0100

    add bootstrap and bootstrap icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap'
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 const app = createApp(App)
 
