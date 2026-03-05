758dee88ccecb2a1fe3b273712b99348cd7349b2
= <=>
2026-03-05 18:29:49 +0100

    update navbar responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,26 +15,26 @@ function increment() {
 
 <template>
   <main class="d-flex flex-column gap-5">
-    <nav class="navbar navbar-expand-lg bg-transparent">
-        <div class="container-fluid bg-transparent">
-            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                <ul class="navbar-nav d-flex justify-content-end w-100">
-                    <li class="nav-item dropdown">
-                        <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            Trier
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-dark">
-                            <li><button class="dropdown-item" @click="sortByLike('desc')">Trier par like (desc)</button></li>
-                            <li><button class="dropdown-item" @click="sortByLike('asc')">Trier par like (asc)</button></li>
-                            <li><button class="dropdown-item" @click="sortByDislike('desc')">Trier par dislike (desc)</button></li>
-                            <li><button class="dropdown-item" @click="sortByDislike('asc')">Trier par dislike (asc)</button></li>
-                            <li><button class="dropdown-item" @click="removeSort">Enlever tri</button></li>
-                        </ul>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
+    <nav class="navbar d-flex justify-content-end navbar-expand-lg bg-transparent">
+      <div class="bg-transparent">
+          <div id="navbarNavDarkDropdown">
+              <ul class="navbar-nav d-flex justify-content-end w-100">
+                  <li class="nav-item dropdown d-flex justify-content-end flex-column align-items-end">
+                      <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                          Trier
+                      </button>
+                      <ul class="dropdown-menu dropdown-menu-dark">
+                          <li><button class="dropdown-item" @click="sortByLike('desc')">Trier par like (desc)</button></li>
+                          <li><button class="dropdown-item" @click="sortByLike('asc')">Trier par like (asc)</button></li>
+                          <li><button class="dropdown-item" @click="sortByDislike('desc')">Trier par dislike (desc)</button></li>
+                          <li><button class="dropdown-item" @click="sortByDislike('asc')">Trier par dislike (asc)</button></li>
+                          <li><button class="dropdown-item" @click="removeSort">Enlever tri</button></li>
+                      </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <div v-for="item in store.slice(0, count)">
       <Home 
